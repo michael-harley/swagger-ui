c2d09db7ff4ddff9e9c670de55232553c273c3c2
michael-harley <michael.harley@traveloka.com>
2021-01-21 13:11:14 +0700

    Customize for FE and BE pagination

---
+++ dist/specs/fe-spec.js
@@ -0,0 +1,8 @@
+const feSpec = {
+    "swagger": "2.0",
+    "info": {
+        "title": "This is FE Spec",
+        "description": "You can barbecue it, boil it, broil it, bake it, sauté it. Dey's uh, Tex-Mex-kabobs, Tex-Mex creole, Tex-Mex gumbo. Pan fried, deep fried, stir-fried. There's pineapple Tex-Mex, lemon Tex-Mex, coconut Tex-Mex, pepper Tex-Mex, Tex-Mex soup, Tex-Mex stew, Tex-Mex salad, Tex-Mex and potatoes, Tex-Mex burger, Tex-Mex sandwich..",
+        "version": "1.0.0"
+    }
+}
